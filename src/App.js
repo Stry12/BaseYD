ff00837bfd7ea901d4dabdedd55bdfb65549fbbd
SalvadorIGO <salvador.gallardo@alumnos.uv.cl>
2023-09-29 15:48:23 -0300

    BackEnd
    Inicio BackEnd 2da entrega

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import cors from 'cors';
+import path from 'path';
+import morgan from 'morgan';
+import express from 'express';
+import { NODE_ENV, RUN_PORT, STATIC_PATH } from './const/conts.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = express();
+
+const corsOptions = {
+    credentials: true,
+    optionSuccessStatus: 200,
+    methods: "GET, PUT, POST, DELETE",
+    origin: '*'
 }
+
+// Archivo de configuración bd
+import './database/connection.js'
+
+App.set('env', NODE_ENV)
+App.set('port', RUN_PORT)
+
+App.use(morgan('dev'));
+App.use(cors(corsOptions));
+App.use(express.json({ limit: '500MB' }));
+App.use(express.urlencoded({ extended: true }));
+
+// Static folder
+App.use(express.static(path.join(path.resolve(), STATIC_PATH)));
+
+// ENDPOINTS
+import routerUser from './routes/user.js';
+
+App.use("/user", routerUser);
 
 export default App;
