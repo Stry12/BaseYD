ed9fcd6057f62e8a9d67d783c0eab7bdb1ceb601
Stry12 <salvador.gallardo@alumnos.uv.cl>
2023-10-21 17:22:52 -0300

    subir imagenes de back end a front end

--- src/controllers/libros.js
+++ src/controllers/libros.js
@@ -1,7 +1,6 @@
 import mysql2 from 'mysql2';
 import connectionConfig from '../database/connection.js';
 
-
 const createConnection = async () => {
     return mysql2.createConnection(connectionConfig);
 }
@@ -10,7 +9,6 @@ const getLibrosTitulo = async (req, res) => {
     try {
         const connection = await createConnection();
         const [rows] = await connection.promise().query('SELECT * FROM libros WHERE Titulo LIKE ?', [`%${req.params.text}%`]);
-
 
         connection.end();
 
@@ -24,8 +22,29 @@ const getLibrosTitulo = async (req, res) => {
     }
 }
 
+const addLibro = async (req, res) => {
+    try {
+        // Verifica si se ha subido un archivo
+        if (!req.file) {
+            return res.status(400).json({ message: 'No se ha subido un archivo de portada.' });
+        }
+
+        // La información de la imagen subida está disponible en req.file
+        const file = req.file;
+        console.log('Nombre del archivo:', file.originalname);
+        console.log('Tipo de archivo:', file.mimetype);
+        console.log('Tamaño del archivo:', file.size);
+
+        // Aquí puedes guardar la información del archivo en la base de datos o realizar otras acciones necesarias.
+
+        return res.status(200).json({ message: 'Información de la imagen recibida con éxito.' });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
 
 
 export const methods = {
-    getLibrosTitulo
+    getLibrosTitulo,
+    addLibro
 };
--- src/routes/libros.js
+++ src/routes/libros.js
@@ -1,8 +1,27 @@
 import { Router } from "express";
 import {methods as librosMethods} from '../controllers/libros.js';
+import { fileURLToPath } from 'url'; // Importa la función fileURLToPath para trabajar con import.meta.url
+import path from 'path';
+import multer from 'multer';
+
+// Obtén la ruta del archivo actual usando import.meta.url y luego conviértela a la ruta del sistema de archivos
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const diskStorage = multer.diskStorage({
+    destination: path.join(__dirname, '../imagenes/portadas'),
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-portada-" + path.extname(file.originalname));
+    }
+});
+
+const fileUpload = multer({
+    storage: diskStorage
+}).single('coverImage');
 
 const router = Router();
 
 router.get("/titulos/:text",librosMethods.getLibrosTitulo);
+router.post("/subir",fileUpload,librosMethods.addLibro);
 
 export default router;
