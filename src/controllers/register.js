79ac37c4f35282bbb98b33ee4386cc80b2d8130b
Mamayoda <129697705+Mamayoda@users.noreply.github.com>
2023-11-14 12:18:14 -0300

    se agrega el bcrypt
    para encriptar contraseñas

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -1,46 +1,55 @@
 import mysql2 from 'mysql2';
 import connectionConfig from '../database/connection.js';
+import bcrypt from 'bcrypt';
 
 const createConnection = async () => {
     return mysql2.createConnection(connectionConfig);
 }
-const existeEmail = async (req,res) => {
-    try {
-        const connection = await createConnection();
-        const [get_email] = await connection.promise().query('SELECT CorreoElectronico FROM usuarios WHERE CorreoElectronico LIKE ? ', [`%${req.params.text}%`]);
-        connection.end();
 
-        console.log(get_email);
+const setUser = async (req, res) => {
+  try {
+    const connection = await createConnection();
 
-        if (get_email.length == 0){
-            return res.status(404).json({message: 'no se encontró el correo.'})
-        }
-        return res.status(200).json({message: 'correo encontrado.'})
-       
-    } catch (error) {
-        return res.status(500).json({ message: error.message });
+    // Verificar si el correo ya existe
+    const [getEmail] = await connection
+      .promise()
+      .query('SELECT CorreoElectronico FROM usuarios WHERE CorreoElectronico LIKE ?', [req.body.email]);
+
+    if (getEmail.length !== 0) {
+      connection.end();
+      return res.status(200).json({ message: 'Este correo ya posee una cuenta.' });
     }
-}
 
-const setUser = async (req,res) => {
-    
-    try {
-        console.log(req.body);
-        const connection = await createConnection();
-        const [getEmail] = await connection.promise().query('SELECT CorreoElectronico FROM usuarios WHERE CorreoElectronico LIKE ?', [req.body.email]);
-        connection.end();
+    // Si el correo no existe, proceder con la inserción
+    const { name, email, password } = req.body;
 
-        if(getEmail.length != 0){
-            return res.status(200).json({message: 'este correo ya posee una cuenta.'});
-        }
-        else{
-            
-        }
+    // Hash de la contraseña
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    } catch (error) {
-        return res.status(404).json({ message: error.message });
+    // Realiza la inserción en la base de datos
+    const [result] = await connection
+      .promise()
+      .query('INSERT INTO usuarios (NombreDeUsuario, CorreoElectronico, Contraseña) VALUES (?, ?, ?)', [
+        name,
+        email,
+        hashedPassword,
+      ]);
+
+    connection.end();
+
+    // Verifica si la inserción fue exitosa
+    if (result.affectedRows === 1) {
+      return res.status(201).json({ message: 'Registro exitoso.' });
+    } else {
+      return res.status(500).json({ message: 'Error al registrar el usuario.' });
     }
-}
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+  // Exporta tus funciones, etc.
+  
 
 
 
